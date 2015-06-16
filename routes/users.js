f2c396b32bf5e279a3ca200f89ca0919faee048c
lenovo <lenovo@localhost.localdomain>
2015-06-16 10:40:30 +0900

    first commit

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+module.exports = router;
